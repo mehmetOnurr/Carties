00caf2664874598a4d36bc15909447953536cd5b
Mehmet Onur Özdemir <85265144+mehmetOnurr@users.noreply.github.com>
2024-07-28 14:47:43 +0300

    first commit

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,58 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Services;
+using SearchService.Data;
+using SearchService.Models;
+using Polly;
+using Polly.Extensions.Http;
+using System.Net;
+using MassTransit;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq((context,cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>{
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    
+    Console.WriteLine(e);
+}
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+       .HandleTransientHttpError()
+       .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+       .WaitAndRetryForeverAsync( _ => TimeSpan.FromSeconds(3));
